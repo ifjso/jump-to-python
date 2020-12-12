54a0e2ef035fedad8804f2f9da5efc8e36d29871
ifjso <ojsboy@hotmail.com>
2020-12-12 19:08:48 +0900

    fix: variables name

--- practice/15.py
+++ practice/15.py
@@ -1,19 +1,19 @@
 s = input('input: ')
-inputtedNumbers  = s.split(' ')
+inputted_numbers  = s.split(' ')
 
 result = [];
-for numbers in inputtedNumbers:
-    existedNumbers = {}
+for numbers in inputted_numbers:
+    existed_numbers = {}
     existed = True
 
     if len(numbers) < 10:
         existed = False
     else:
         for n in numbers:
-            if n in existedNumbers:
+            if n in existed_numbers:
                existed = False
                break
-            existedNumbers[n] = n
+            existed_numbers[n] = n
 
     result.append(existed)
 
